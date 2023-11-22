11d39379c6baf9b95670295e5fd6d634d2ce796b
Tuhin Bepari <digitaldreams40@gmail.com>
2023-11-22 13:11:13 +0600

    working with Service Binding. ProductRepository

--- src/Service/CreateProductService.php
+++ src/Service/CreateProductService.php
@@ -9,7 +9,7 @@ class CreateProductService
 {
 
     public function __construct(
-        protected ProductRepository $repository,
+        protected ProductRepository $inMemoryProductRepository,
         protected NewProductCreatedNotification $productCreatedNotification
     ) {
     }
@@ -17,7 +17,7 @@ class CreateProductService
     public function save(array $data)
     {
         $product = new Product(id: null, name: $data['name'], price: $data['price'], status: 'created');
-        $this->repository->save($product);
+        $this->inMemoryProductRepository->save($product);
         $this->productCreatedNotification->send($product);
 
         return $product;
